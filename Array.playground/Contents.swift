d3e667762f807c55c9d0b0164408cab76c38751e
teacher <phrombutr@gmail.com>
2017-06-13 13:39:46 +0700

    Add Array

--- Array.playground/Contents.swift
+++ Array.playground/Contents.swift
@@ -22,7 +22,12 @@ var douArrayNub4 = [Double]()
 douArrayNub4 = [2.1,2.2,2.3]
 
 
+//การเพิ่มสมาชิคให้ Array
+print("intArrayNumber2 ==> \(intArrayNumber2)")
+intArrayNumber2.append(111)
 
+strArrayName2
+strArrayName2.append("Doramon")
 
 
 
