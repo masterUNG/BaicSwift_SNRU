9c66708fb23a6f4f3ea8261d91675b24ab20d375
teacher <phrombutr@gmail.com>
2017-06-13 13:57:11 +0700

    Array Success

--- Array.playground/Contents.swift
+++ Array.playground/Contents.swift
@@ -30,6 +30,23 @@ strArrayName2
 strArrayName2.append("Doramon")
 
 
+//การหา ขนาดของ Array
+strArrayName2.count
+
+//นี่คือการเรียกใช้ค่าจาก Array โดยใช้ index
+print("strArray2 ที่ index = 3 มีค่า \(strArrayName2[3])")
+
+//การลบสมาชิค ของ Array
+strArrayName2
+strArrayName2.remove(at: 1)
+print(strArrayName2)
+
+
+//การเรียง สมาชิก Array
+var intA = [9,5,7,1,3,10]
+intA.sort()
+print(intA)
+
 
 
 
