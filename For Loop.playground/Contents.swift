af27f69cb26c3eb520aab3c625aefbd6b3a40bb6
teacher <phrombutr@gmail.com>
2017-06-14 13:39:20 +0700

    for Loop

---
+++ For Loop.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Implicit
+let intNumber = [11,22,33,44,55]
+var intIndex = 0
+
+// นี่คือพื้นฐาน ของ For Loop
+for intArray in intNumber {
+
+    print("inArray ==> (\(intIndex)) = \(intArray)")
+    intIndex += 1
+    
+}
+
+//For Loot ใช้กับ String
+let strArrayName = ["AAA", "BBB", "CCC", "DDD"]
+for strMyName in strArrayName {
+
+    print("strMyName ==> \(strMyName)")
+
+}
+
+
+
+
+
